ada1a61e1b58b5caf12e77737c263e9dab367fc1
Brandon Meyer <bmeyerkc@gmail.com>
2013-03-19 00:50:39 -0500

    Removing dependency on Meteorite. For now.

--- client/MatchRouter.js
+++ client/MatchRouter.js
@@ -1,24 +1,31 @@
-var MatchRoute = function( ) {
-  var self = this;
-  Matches.bindTemplate("matchPage", { liveUpdate: true } );
-  Matches.bindTemplate("matchCreate");
-  self.allMatches = function( query, page ) {
-		console.log("Match Router - allMatches");
-		var html = Meteor.render( function( ) {
-			return Template.matchPage( { matches: self.matches} );
-		} );
-    	$("#main").html( html );	
-	};
-	self.createMatch = function( query, page ) {
-	  console.log("Match Router = createMatch");
-	  var html = Meteor.render( function( ) {
-	      return Template.matchCreate( );
-	  } );
-	  $("#main").html( html );
-    $(".datetimepicker").datetimepicker( {
-      language: 'en',
-      pick12HourFormat: true    } );
-	};
-	self.matches = Matches.find( );
+var MatchRoute = function () {
+    var self = this;
+    Matches.bindTemplate("matchPage", { liveUpdate: true });
+    Matches.bindTemplate("matchCreate");
+    //allMatches
+    self.allMatches = function (query, page) {
+        console.log("Match Router - allMatches");
+        var html = Meteor.render(function () {
+            return Template.matchPage({ matches: self.matches});
+        });
+        $("#main").html(html);
+    };
+    //createMatch
+    self.createMatch = function (query, page) {
+        console.log("Match Router - createMatch");
+        var html = Meteor.render(function () {
+            return Template.matchCreate();
+        });
+        $("#main").html(html);
+        $(".datetimepicker").datetimepicker({
+            language: 'en',
+            pick12HourFormat: true    });
+    };
+    //load
+    self.load = function (ctx, page) {
+        var id = ctx.params.id;
+        console.log("Match Router - load: " + id);
+    };
+    self.matches = Matches.find();
 };
 
--- pummelyou.js
+++ pummelyou.js
@@ -1,65 +1,87 @@
 Matches = new Meteor.Collection("matches");
-var sayConsole = function( message ) {
-  console.log( message );
+var sayConsole = function (message) {
+    console.log(message);
 };
 //
 //---- SERVER ----
 //
-if( Meteor.isServer ) {
-  Meteor.publish("matches", function( ) {
-      return Matches.find( );
-  } );
-  Matches.allow( {
-      insert: function( userId, match ) {
-        return true;
-      },
-      update: function( userId, match ) {
-        if( userId == undefined || userId == null) return false;
-        return true;
-      }
-  } );
-  Meteor.startup( function( ) {
-      sayConsole("server startup");
-  } );
+if (Meteor.isServer) {
+    Meteor.publish("matches", function () {
+        return Matches.find();
+    });
+    Matches.allow({
+        insert: function (userId, match) {
+            return true;
+        },
+        update: function (userId, match) {
+            if (userId == undefined || userId == null) return false;
+            return true;
+        }
+    });
+    Meteor.startup(function () {
+        sayConsole("server startup");
+    });
 }
 //
 //---- CLIENT ----
 //
-if( Meteor.isClient ) {
-  Meteor.subscribe("matches");
-  Meteor.startup( function( ) {
-      $( function( ) {
-          // Define Minimongo collections to match server/publish.js.
-          //Matches = new Meteor.Collection( "matches" );
-          // Define routing
-          var defaultController = new DefaultRoute( ); 
-          var matchController = new MatchRoute( );
-          page( "/", defaultController.defaultRoute ); 
-          page( "/match", matchController.allMatches ); 
-          page( "/match/create", matchController.createMatch );
-          page( );
-      } );
-  } );
-  //Template Setups
-  Template.hello.greeting = function( ) { 
-    return "Welcome to pummelyou."; 
-  };
-  Template.scoresTop.matchGroup = function( ) {
-            return Matches.find( );
-          };
-  Template.scoresTop.helpers({
-      gameTimeShort: function (match) {
-        if (match.gameTime == null) {
-          return "BAD";
+if (Meteor.isClient) {
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_AND_EMAIL"
+    });
+
+
+    Meteor.subscribe("matches");
+    Meteor.startup(function () {
+        $(function () {
+            // Define Minimongo collections to match server/publish.js.
+            //Matches = new Meteor.Collection( "matches" );
+            // Define routing
+            var defaultController = new DefaultRoute();
+            var matchController = new MatchRoute();
+            page("/", defaultController.defaultRoute);
+            page("/match", matchController.allMatches);
+            page("/match/create", matchController.createMatch);
+            page("/match/:id", matchController.load, matchController.show);
+
+            page();
+        });
+    });
+    //Handlebar Helpers
+    Handlebars.registerHelper("underscore", function (badString) {
+        var goodString = badString.replace(" ", "_");
+        return goodString;
+    });
+    //Template Setups
+    Template.hello.greeting = function () {
+        return "Welcome to pummelyou.";
+    };
+    Template.scoresTop.matchGroup = function () {
+        var matches = Matches.find({});
+        var gameTypes = [ ];
+        matches.forEach(function (match) {
+            if (gameTypes.indexOf(match.game) == -1) {
+                gameTypes.push(match.game);
+            }
+        });
+        return gameTypes;
+    };
+    Template.scoresTop.matches = function () {
+        var gameType = this.toString();
+        var matches = Matches.find({"game": gameType });
+        return matches;
+    };
+    Template.scoresTop.helpers({
+        gameStatus: function (match) {
+            if (match.gameTime == null) {
+                return "BAD";
+            }
+            var d = Date.parse(match.gameTime);
+            return d.toString("ddd HH:mm tt");
         }
-        var d = Date.parse(match.gameTime);
-        return d.toString("ddd HH:mm tt");
-      }
-  });
-  
-  //matchCreate
-  Template.matchCreate.rendered = function() {
-  };
-  
+    });
+    //matchCreate
+    Template.matchCreate.rendered = function () {
+    };
 } 
 
